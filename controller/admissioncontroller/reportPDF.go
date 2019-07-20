f29a4bc93479aa3238031757c2ce5c644f4c9571
yoel antezana <contacto@paulantezana.com>
2019-07-20 09:32:42 -0500

    new updates

--- controller/admissioncontroller/reportPDF.go
+++ controller/admissioncontroller/reportPDF.go
@@ -3,6 +3,7 @@ package admissioncontroller
 import (
     "crypto/sha256"
     "fmt"
+    "github.com/dgrijalva/jwt-go"
     "github.com/jung-kurt/gofpdf"
     "github.com/labstack/echo"
     "github.com/paulantezana/review/config"
@@ -29,6 +30,11 @@ type licenceADF struct {
 }
 
 func GetPDFAdmissionStudentLicense(c echo.Context) error {
+    // Get user token authenticate
+    user := c.Get("user").(*jwt.Token)
+    claims := user.Claims.(*utilities.Claim)
+    currentUser := claims.User
+
     // Get data request
     request := utilities.Request{}
     if err := c.Bind(&request); err != nil {
@@ -59,8 +65,14 @@ func GetPDFAdmissionStudentLicense(c echo.Context) error {
         return c.JSON(http.StatusOK, utilities.Response{Message: fmt.Sprintf("%s", err)})
     }
 
+    // Query Subsidiary
+    subsidiary := models.Subsidiary{}
+    if err := DB.First(&subsidiary,models.Subsidiary{ ID: request.SubsidiaryID }).Error; err != nil {
+        return c.JSON(http.StatusOK, utilities.Response{Message: fmt.Sprintf("%s", err)})
+    }
+
     // Settings
-    pageMargin := 12.7
+    pageMargin := 9.0
 
     // Create PDF
     pdf := gofpdf.New("P", "mm", "A4", "")
@@ -83,50 +95,85 @@ func GetPDFAdmissionStudentLicense(c echo.Context) error {
     pdf.AddPage()
     pdf.SetFont(fontFamilyName, "B", 10)
 
-    // Background
-    pdf.Image("static/backgroundPattern1.jpg", 0, 0, 110, 110, false, "", 0, "")
-    //pdf.Image("static/backgroundPattern1.jpg", 110, 0, 110, 110, false, "", 0, "")
-    //pdf.Image("static/backgroundPattern1.jpg", 0, 110, 110, 110, false, "", 0, "")
-    //pdf.Image("static/backgroundPattern1.jpg", 110, 110, 110, 110, false, "", 0, "")
-
-    gCols := 3.0
+    gCols := 2.0
     cCol := 0.0
     cRow := 0.0
+    padding := 4.0
+
     for _, license := range admissionLicences {
-        headerString := fmt.Sprintf("%s \n %s - Sicuani",setting.Prefix, setting.Institute )
+        w := (pageWidth - (gutter * (gCols - 1))) / gCols
+        h := 56.0
+
+        x :=  leftMargin + ( w * cCol) + ( gutter * cCol)
+        y :=  topMargin + ( h * cRow) + ( gutter * cRow)
+
+        // Background
+        if utilities.FileExist("static/backgroundMachupicchu.jpg") {
+            pdf.Image("static/backgroundMachupicchu.jpg", x, y, w, h, false, "", 0, "")
+        }
+
+        // Header Images
+        pdf.Image(setting.NationalEmblem, x + padding, y + padding, 9, 0, false, "", 0, "")
+        pdf.Image(setting.Logo, (x + w) - (9 + padding), y + 4, 9, 0, false, "", 0, "")
+
+        // Header content
+        pdf.SetFont(fontFamilyName, "B", 10)
+        pdf.SetXY(x + 9 + padding, y + padding)
+        headerString := fmt.Sprintf("%s \n %s - %s CARNET DEL POSTULANTE", setting.Prefix, setting.Institute, subsidiary.District )
         headerString = strings.ToUpper(headerString)
-        pdf.Image(setting.NationalEmblem, leftMargin, topMargin, 9, 0, false, "", 0, "")
-        pdf.SetX(leftMargin + 9 + gutter)
-        pdf.MultiCell(70,3.5,headerString,"","C",false)
-        pdf.Image(setting.Logo, leftMargin + 79 + (gutter * 2), topMargin, 9, 0, false, "", 0, "")
+        pdf.MultiCell(w - (18 + ( padding * 2 )),3.5, headerString,"","C",false)
 
         // Profile
         if utilities.FileExist(license.Avatar) {
-            pdf.Image(license.Avatar, leftMargin, topMargin + 15, 25, 0, false, "", 0, "")
+           pdf.Image(license.Avatar, x + padding, y + 15 + padding, 25, 0, false, "", 0, "")
         }
 
+        // Content
+        pdf.SetXY(x + padding + 27, y + padding + 15 )
+        pdf.Cell(30,3.5,"DNI:")
+        pdf.Cell(30,3.5,"ADMISION:")
 
+        pdf.SetXY(x + padding + 27, y + padding + 18.5 )
+        pdf.SetFont(fontFamilyName, "", 10)
+        pdf.Cell(30,3.5,license.DNI)
+        pdf.Cell(30,3.5,fmt.Sprintf("%d",license.Year))
 
-        rW := (pageWidth / gCols) - (gutter * (gCols - 1))
-        rH := 50.0
+        // Name
+        pdf.SetXY(x + padding + 27, y + padding + 25 )
+        pdf.SetFontStyle("B")
+        pdf.Cell(30,3.5,"APELLIDOS Y NOMBRES:")
 
-        sX :=  leftMargin
-        if cCol > 0.0 {
-            sX = (rW * cCol) + leftMargin + (gutter * cCol)
-        }
+        pdf.SetXY(x + padding + 27, y + padding + 28.5 )
+        pdf.SetFontStyle("")
+        pdf.Cell(30,3.5,license.FullName)
 
-        sY := topMargin
-        if cRow == 0.0 {
-            sY = topMargin
-        }else {
-            sY = (rH * cRow) + topMargin + gutter
-        }
+        // Program
+        pdf.SetXY(x + padding + 27, y + padding + 35 )
+        pdf.SetFontStyle("B")
+        pdf.Cell(30,3.5,"PROGRAMA DE ESTUDIOS:")
 
+        pdf.SetXY(x + padding + 27, y + padding + 38.5 )
+        pdf.SetFontStyle("")
+        pdf.Cell(30,3.5,license.Program)
 
-        pdf.Rect(sX,sY,rW,rH,"")
+        // Position
+        pdf.SetXY(x + padding + 27, y + padding + 45 )
+        pdf.SetFontStyle("B")
+        pdf.Cell(10,3.5,"AULA: ")
+        pdf.SetFontStyle("")
+        pdf.Cell(15,3.5,fmt.Sprintf("%d",license.Classroom))
+        pdf.SetFontStyle("B")
+        pdf.Cell(17,3.5,"NUMERO: ")
+        pdf.SetFontStyle("")
+        pdf.Cell(15,3.5,fmt.Sprintf("%d",license.Seat))
+
+        // Rect
+        pdf.SetFillColor(200,200,200)
+        pdf.SetDrawColor(200,200,200)
+        pdf.Rect(x,y,w,h,"")
 
         // Set new params
-        if cCol < (gCols - 1) {
+        if cCol < ( gCols - 1 ) {
             cCol += 1
         }else {
             cCol = 0.0
@@ -135,9 +182,9 @@ func GetPDFAdmissionStudentLicense(c echo.Context) error {
     }
 
     // Set file name
-    cc := sha256.Sum256([]byte(time.Now().String()))
+    cc := sha256.Sum256([]byte(fmt.Sprintf("%d-%d",request.IDs, currentUser.ID )))
     pwd := fmt.Sprintf("%x", cc)
-    fileName :=  fmt.Sprintf("static/rpe/%s.pdf",pwd)
+    fileName :=  fmt.Sprintf("static/rpe/admission/%s.pdf",pwd)
 
     // Save file
     err := pdf.OutputFileAndClose(fileName)
@@ -153,6 +200,11 @@ func GetPDFAdmissionStudentLicense(c echo.Context) error {
 }
 
 func GetPDFAdmissionStudentList(c echo.Context) error {
+    // Get user token authenticate
+    user := c.Get("user").(*jwt.Token)
+    claims := user.Claims.(*utilities.Claim)
+    currentUser := claims.User
+
     // Get data request
     admission := models.Admission{}
     if err := c.Bind(&admission); err != nil {
@@ -172,9 +224,9 @@ func GetPDFAdmissionStudentList(c echo.Context) error {
     pdf.AddPage()
 
     // Set file name
-    cc := sha256.Sum256([]byte(time.Now().String()))
+    cc := sha256.Sum256([]byte(fmt.Sprintf("%d-%d",admission.ID, currentUser.ID )))
     pwd := fmt.Sprintf("%x", cc)
-    fileName :=  fmt.Sprintf("static/rpe/%s.pdf",pwd)
+    fileName :=  fmt.Sprintf("static/rpe/admission/%s.pdf",pwd)
 
     // Save file
     err := pdf.OutputFileAndClose(fileName)
@@ -190,6 +242,11 @@ func GetPDFAdmissionStudentList(c echo.Context) error {
 }
 
 func GetPDFAdmissionStudentFile(c echo.Context) error {
+    // Get user token authenticate
+    user := c.Get("user").(*jwt.Token)
+    claims := user.Claims.(*utilities.Claim)
+    currentUser := claims.User
+
     // Get data request
     request := utilities.Request{}
     if err := c.Bind(&request); err != nil {
@@ -266,11 +323,10 @@ func GetPDFAdmissionStudentFile(c echo.Context) error {
     pdf.CellFormat(15, 6, "AÑO", "1", 0, "C", false, 0, "")
     pdf.CellFormat(30, 6, "2019", "1", 0, "C", false, 0, "")
 
-
     // Set file name
-    cc := sha256.Sum256([]byte(time.Now().String()))
+    cc := sha256.Sum256([]byte(fmt.Sprintf("%d-%d",request.IDs, currentUser.ID )))
     pwd := fmt.Sprintf("%x", cc)
-    fileName :=  fmt.Sprintf("static/rpe/%s.pdf",pwd)
+    fileName :=  fmt.Sprintf("static/rpe/admission/%s.pdf",pwd)
 
     // Save file
     err := pdf.OutputFileAndClose(fileName)
